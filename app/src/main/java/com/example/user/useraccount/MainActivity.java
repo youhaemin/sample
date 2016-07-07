a8273819977fac57d8a9b503b94a387d4eec025c
user <haey0629@hanmail.net>
2016-07-07 17:03:57 +0900

    최초등록

---
+++ app/src/main/java/com/example/user/useraccount/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.user.useraccount;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+//로그인 버틍늘 입력하면 입력된아이디와 비밀번호를 얻어와 토스트로 출력
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText id;
+    EditText pwd;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        id = (EditText) findViewById(R.id.id);
+        pwd = (EditText) findViewById(R.id.pwd);
+    }
+
+    public void onButtonClicked(View v) {
+        Intent i = new Intent(this, SubActivity.class);
+        String strId = id.getText().toString();
+        String strPwd = pwd.getText().toString();
+    }
+}
